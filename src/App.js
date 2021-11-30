b9d08f0646ff014914f5c19aea9099d7d5e6a026
Leomar Romanzini <leomarromanzini@gmail.com>
2021-11-30 10:31:13 -0300

    add: directorys

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import { ProSidebar } from "react-pro-sidebar";
-import "react-pro-sidebar/dist/css/styles.css";
+import Aside from "./components/Aside";
 import { GlobalStyles } from "./styles/global";
 
 function App() {
   return (
     <>
+      <Aside />
       <GlobalStyles />
-      <p>oiadadasdadadd</p>
-      <ProSidebar
-        style={{ height: "100vh" }}
-        image="https://wallpaperaccess.com/full/124383.jpg"
-      >
-        {/* <Menu iconShape="square">
-          <MenuItem icon={<FaGem />}>Dashboard</MenuItem>
-          <SubMenu title="Components" icon={<FaHeart />}>
-            <MenuItem>Component 1</MenuItem>
-            <MenuItem>Component 2</MenuItem>
-          </SubMenu>
-        </Menu> */}
-      </ProSidebar>
     </>
   );
 }
